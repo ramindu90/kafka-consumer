214e6dedef59dec2e1171028cd922965bad2b630
ramindu <rami.desilva@gmail.com>
2020-10-16 18:52:44 +0530

    Add kafka throughput consumer

--- src/main/java/kafka/KafkaConsumer.java
+++ src/main/java/kafka/KafkaConsumer.java
@@ -3,13 +3,17 @@ package kafka;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.kafka.clients.consumer.CommitFailedException;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.common.TopicPartition;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by ramindu on 1/9/17.
@@ -47,7 +51,7 @@ public class KafkaConsumer implements Runnable  {
         props.put("session.timeout.ms", "30000");
         props.put("enable.auto.commit", "true");
         props.put("auto.offset.reset", "earliest");
-//        props.put("enable.auto.commit", "true");
+        props.put("enable.auto.commit", "true");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         consumer = new org.apache.kafka.clients.consumer.KafkaConsumer<>(props);
@@ -65,9 +69,15 @@ public class KafkaConsumer implements Runnable  {
     public void run() {
         long startTime = System.currentTimeMillis();
         long endTime = System.currentTimeMillis() + timeDuration;
-        long TPSRecords = 0;
-        long totalRecordCount = 0;
+        AtomicLong TPSRecords = new AtomicLong(0);
+        AtomicLong totalRecordCount = new AtomicLong(0);
+        System.out.println("Starting time: " + (new Date()));
         System.out.println("running !!!!!!!!!! " + endTime + " > " + System.currentTimeMillis());
+        long fromSourceToThrouputClient = 0;
+        long fromSinkToThroughputClient = 0;
+        long fromSourcetoSink = 0;
+        long fromThroughputClientToSource = 0;
+        long fullLatency = 0;
         while (endTime > System.currentTimeMillis()) {
             ConsumerRecords<byte[], byte[]> records = null;
             try {
@@ -77,28 +87,56 @@ public class KafkaConsumer implements Runnable  {
             } catch (CommitFailedException ex) {
                 System.out.println("Consumer poll() failed." + ex.getMessage());
             }
+            long currentTimestamp = System.currentTimeMillis();
             if (null != records) {
                 long recCount = records.count();
-                TPSRecords += recCount;
-                totalRecordCount += recCount;
-                if (startTime + 1000 < System.currentTimeMillis()) {
-                    System.out.println("Receiving throughput per second is: " + TPSRecords +
-                            " totalReceivedCount: " + totalRecordCount);
-                    TPSRecords = 0;
-                    startTime = System.currentTimeMillis();
+                TPSRecords.addAndGet(recCount);
+                totalRecordCount.addAndGet(recCount);
+                for (ConsumerRecord record : records) {
+                    JSONObject metaJsonObject = new JSONObject(record.value().toString()).getJSONObject("metadata");
+                    fromThroughputClientToSource +=
+                            Long.parseLong(metaJsonObject.getString("consumer_out_timestamp")) -
+                                    Long.parseLong(metaJsonObject.getString("consumer_in_timestamp"));
+                    fromSourceToThrouputClient +=
+                            currentTimestamp - Long.parseLong(metaJsonObject.getString("consumer_out_timestamp"));
+                    fromSinkToThroughputClient +=
+                            currentTimestamp - record.timestamp();
+                    fromSourcetoSink +=
+                            record.timestamp() - Long.parseLong(metaJsonObject.getString("consumer_out_timestamp"));
+                    fullLatency +=
+                            currentTimestamp - Long.parseLong(metaJsonObject.getString("consumer_in_timestamp"));
                 }
-//                try {
-//                    if (!records.isEmpty()) {
-//                        consumer.commitAsync();
-//                    }
-//                } catch (CommitFailedException e) {
-//                    log.error("Kafka commit failed for topic kafka_result_topic", e);
-//                }
+
+                if (startTime + 1000 < currentTimestamp) {
+                    if (TPSRecords.get() != 0) {
+                        System.out.println(
+                                "Receiving TPS: " + TPSRecords.get() + " totalReceivedCount: " + totalRecordCount.get() +
+                                        " fromThroughputClientToSource: " + fromThroughputClientToSource/TPSRecords.get() +
+                                        " fromSourceToThrouputClient: " + fromSourceToThrouputClient/TPSRecords.get() +
+                                        " fromSinkToThroughputClient: " + fromSinkToThroughputClient/TPSRecords.get() +
+                                        " fromSourcetoSink: " + fromSourcetoSink/TPSRecords.get() +
+                                        " fullLatency: " + fullLatency/TPSRecords.get());
+                    } else {
+                        System.out.println("No event recieved. totalReceivedCount: " + totalRecordCount);
+                    }
+                    TPSRecords = new AtomicLong(0);
+                    startTime = currentTimestamp;
+                    fromSourceToThrouputClient = 0;
+                    fromSinkToThroughputClient = 0;
+                    fromThroughputClientToSource = 0;
+                    fromSourcetoSink = 0;
+                    fullLatency = 0;
+                }
             } else {
-                if (startTime + 1000 < System.currentTimeMillis()) {
+                if (startTime + 1000 < currentTimestamp) {
                     System.out.println("No event recieved. totalReceivedCount: " + totalRecordCount);
-                    TPSRecords = 0;
-                    startTime = System.currentTimeMillis();
+                    TPSRecords = new AtomicLong(0);
+                    startTime = currentTimestamp;
+                    fromSourceToThrouputClient = 0;
+                    fromSinkToThroughputClient = 0;
+                    fromThroughputClientToSource = 0;
+                    fromSourcetoSink = 0;
+                    fullLatency = 0;
                 }
             }
             try { //To avoid thread spin
@@ -108,5 +146,6 @@ public class KafkaConsumer implements Runnable  {
                 Thread.currentThread().interrupt();
             }
         }
+        System.out.println("Ending time: " + (new Date()));
     }
 }
